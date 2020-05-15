3fca20a28f8e0490fe63c1dac31c3d13a9b787d3
Ricardo O <guillermo.olea@amovildigital.com>
2020-05-15 10:04:05 -0500

    Initial Commit

---
+++ CeroCaries/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CeroCaries
+//
+//  Created by Ricardo Olea on 15/05/20.
+//  Copyright © 2020 Ricardo Olea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
