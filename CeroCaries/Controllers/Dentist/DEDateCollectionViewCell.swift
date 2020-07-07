66073541b439612109bc6e01e57c0053b844db51
Ricardo O <guillermo.olea@amovildigital.com>
2020-07-07 13:42:19 -0500

    Add dentist Calendar

---
+++ CeroCaries/Controllers/Dentist/DEDateCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  DEDateCollectionViewCell.swift
+//  CeroCaries
+//
+//  Created by Ricardo Olea on 06/07/20.
+//  Copyright © 2020 Ricardo Olea. All rights reserved.
+//
+
+import UIKit
+
+class DEDateCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var dateImage: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+}
--- CeroCaries/Controllers/LogIn/LNCategoryViewController.swift
+++ CeroCaries/Controllers/LogIn/LNCategoryViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class LNCategoryViewController: BaseViewController {
-
+    
+    @IBOutlet weak var dentistBtn: UIButton!
+    @IBOutlet weak var individualBtn: UIButton!
+    @IBOutlet weak var schoolBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,8 +21,34 @@ class LNCategoryViewController: BaseViewController {
        disableDarkMode()
        //NavigationBarConfig
        imageNavBar()
+       //ConfigStyleView
+       viewStyle()
+
+        
+    }
+    
+    func viewStyle(){
+        
+        _ = configButton(button: dentistBtn)
+        _ = configButton(button: individualBtn)
+        _ = configButton(button: schoolBtn)
+        
+    }
+    
+    func configButton(button: UIButton) -> UIButton{
+        button.layer.borderWidth = 1.5
+        button.layer.cornerRadius = 17
+        button.layer.borderColor = UIColor(red: 68.0/255, green: 68.0/255, blue: 115.0/255, alpha: 1.0).cgColor
+        return button
     }
     
 
+    @IBAction func actionDentist(_ sender: Any) {
+        
+        dentistBtn.setTitleColor(.white, for: .normal)
+        
+        dentistBtn.backgroundColor = UIColor(red: 33/255, green: 68/255, blue: 115/255, alpha: 1)
+    }
+    
 
 }
