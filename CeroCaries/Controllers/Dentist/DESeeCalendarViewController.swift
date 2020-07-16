ad4e5304663c967f224a01d8aab7dbc244a558b0
Ricardo O <guillermo.olea@amovildigital.com>
2020-07-15 22:58:21 -0500

    Add Dentist Today Calendar

--- CeroCaries/Controllers/Dentist/DESeeCalendarViewController.swift
+++ CeroCaries/Controllers/Dentist/DESeeCalendarViewController.swift
@@ -8,64 +8,237 @@
 
 import UIKit
 
-class DESeeCalendarViewController: BaseViewController {
+class DESeeCalendarViewController: BaseViewController, UICollectionViewDelegate,UICollectionViewDataSource {
     
-    var patientList = [String]()
-    var hoursList = ["10:00 am a 11:00 am",
-                     "11:00 am a 12:00 pm",
-                     "12:00 pm a 11:00 am",
-                     "10:00 am a 1:00 pm",
-                     "2:00 pm a 3:00 pm",
-                     "4:00 pm a 5:00 pm",
-                     "5:00 pm a 6:00 pm",
-                     "6:00 pm a 7:00 pm",]
+    @IBOutlet weak var viewCalendar: UIView!
+    @IBOutlet weak var calendarCollection: UICollectionView!
+    @IBOutlet weak var monthLabel: UILabel!
+    
+    let Months = ["ENERO", "FEBRERO", "MARZO", "ABRIL", "MAYO", "JUNIO",
+                  "JULIO", "AGOSTO", "SEPTIEMBRE", "OCTUBRE", "NOVIEMBRE", "DICIEMBRE"]
+    let DaysOfMonth = ["Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sabado"]
+    var DaysInMonths = [31,28,31,30,31,30,31,31,30,31,30,31]
+    //year 2012,2016,2020 ..
+    var DaysInMonthsBisiesto = [31,29,31,30,31,30,31,31,30,31,30,31]
+    var currentMonth = String()
+    var NumberOfEmptyBox = Int() //number of empty boxes atthe start the current month
+    var NextNumberOfEmptyBox =  Int() //the same the above with next moth
+    var PreviousNumberOfEmptyBox = 0 //the sabe the above with prev month
+    var Direction = 0 // = 0 if we are at the current month. = 1 if we are  in a future month,= -1 if we are  in a pasr month
+    var PositionIndex = 0//heree we will store the above vars of the empty boxes
+    var LeapYearCounter =  4 //its 2 becausethe next time february has 29 days is in two years(it happens every 4 years)
+    var dayCounter = 0
+    var highligthDate = 20
+    
+    var DaysHighLight = ["2 ENERO 2020","10 JUNIO 2020","1 AGOSTO 2020","6 AGOSTO 2020","7 AGOSTO 2020","2 AGOSTO 2020", "30 AGOSTO 2020"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        sectionData()
-
-       
-    }
-    
-    func sectionData(){
-           patientList.append("Sergio Gómez")
-           patientList.append("Raúl Pérez Ortíz")
-           patientList.append("Mariana Brito")
-           patientList.append("Miriam Lira")
-           patientList.append("Alberto Torres")
-           patientList.append("DISPONIBLE")
-           patientList.append("Laura Morales")
-           patientList.append("Luis Perez")
-       }
-    @IBAction func backAction(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
-    }
-    
-
-   
-
-}
-// MARK: - UITableView
-extension DESeeCalendarViewController: UITableViewDelegate, UITableViewDataSource{
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return hoursList.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "seeCalendarCell") as! DESeeCalendarTableViewCell
-        cell.hourLabel.text = hoursList[indexPath.row]
-        cell.patientLabel.text = patientList[indexPath.row]
-        if indexPath.row == 5{
-            cell.hourLabel.textColor = .gray
-            cell.patientLabel.textColor = .gray
-           
+        
+        currentMonth = Months[month]
+        print("fecha\(currentMonth)\(year)")
+        monthLabel.text = "\(currentMonth) \(year)"
+        if weekday == 0 {
+            weekday = 7
         }
-        return cell
+        if (year%4 == 0){
+            DaysInMonths  = [31,29,31,30,31,30,31,31,30,31,30,31]
+        }else{
+            DaysInMonths  = [31,28,31,30,31,30,31,31,30,31,30,31]
+        }
+        GetStartDateDayPosition()
+        
     }
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 60
+    func GetStartDateDayPosition(){  //this functions gives us the number of empty boxes
+              switch Direction{
+              case 0:// if we are at the current month
+                  NumberOfEmptyBox = weekday
+                  dayCounter = day
+                  while dayCounter > 0{
+                      NumberOfEmptyBox = NumberOfEmptyBox - 1
+                      dayCounter = dayCounter - 1
+                      if NumberOfEmptyBox == 0 {
+                          NumberOfEmptyBox = 7
+                      }
+                  }
+                  if NumberOfEmptyBox == 7 {
+                      NumberOfEmptyBox = 0
+                  }
+                  PositionIndex = NumberOfEmptyBox
+                  
+              case 1...://if we are at the future month
+                  NextNumberOfEmptyBox = (PositionIndex + DaysInMonths[month])%7
+                  PositionIndex = NextNumberOfEmptyBox
+              
+              case  -1://if we are at the pasr month
+                  PreviousNumberOfEmptyBox = (7 - (DaysInMonths[month] - PositionIndex)%7)
+                  if PreviousNumberOfEmptyBox == 7{
+                      PreviousNumberOfEmptyBox = 0
+                  }
+                  PositionIndex = PreviousNumberOfEmptyBox
+              default:
+                  fatalError()
+              }
+          }
+          
+          @IBAction func nextMonth(_ sender: Any) {
+              //highligthDate = -1
+              switch currentMonth{
+              case "DICIEMBRE":
+                  month = 0
+                  year += 1
+                  Direction = 1
+          
+                  if (year%4 == 0){
+                      DaysInMonths  = [31,29,31,30,31,30,31,31,30,31,30,31]
+                      
+                  }else{
+                       DaysInMonths  = [31,28,31,30,31,30,31,31,30,31,30,31]
+                  }
+                  GetStartDateDayPosition()
+                  currentMonth  = Months[month]
+                  monthLabel.text = "\(currentMonth) \(year)"
+                  calendarCollection.reloadData()
+              default:
+                  //month += 1
+                  Direction   = 1
+                  GetStartDateDayPosition()
+                  month += 1
+                  currentMonth  = Months[month]
+                  monthLabel.text = "\(currentMonth) \(year)"
+                  calendarCollection.reloadData()
+                  
+              }
+          }
+          
+          @IBAction func prevMonth(_ sender: Any) {
+              
+              //highligthDate = -1
+              switch currentMonth{
+              case "ENERO":
+                  month = 11
+                  year -= 1
+                  Direction = -1
+
+                  if (year%4 == 0){
+                      DaysInMonths  = [31,29,31,30,31,30,31,31,30,31,30,31]
+                      
+                  }else{
+                       DaysInMonths  = [31,28,31,30,31,30,31,31,30,31,30,31]
+                  }
+                  GetStartDateDayPosition()
+                  currentMonth  = Months[month]
+                  monthLabel.text = "\(currentMonth) \(year)"
+                  calendarCollection.reloadData()
+              default:
+                  month -= 1
+                  Direction = -1
+                  GetStartDateDayPosition()
+                  currentMonth = Months[month]
+                  monthLabel.text = "\(currentMonth) \(year)"
+                  calendarCollection.reloadData()
+                  
+              }
+          }
+          
+          
+          func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+              switch Direction{//it return the number of days in the moth + the numeber of empty boxes baserd on the direction we are going
+              case 0 :
+                  return DaysInMonths[month] + NumberOfEmptyBox
+              case 1...:
+                  return DaysInMonths[month] + NextNumberOfEmptyBox
+              case -1:
+                  return DaysInMonths[month] + PreviousNumberOfEmptyBox
+              default:
+                  fatalError()
+
+              }
+          }
+          
+          func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+              let cell  = collectionView.dequeueReusableCell(withReuseIdentifier: "DESeeCalendarCell", for: indexPath) as! DESeeCalendarCollectionViewCell
+              cell.backgroundColor = UIColor.clear
+              
+              cell.dateLabel.textColor = UIColor.black
+              
+              if cell.isHidden{
+                  cell.isHidden = false
+              }
+              
+              switch Direction{
+              case 0 :
+                  cell.dateLabel.text = "\(indexPath.row + 1 - NumberOfEmptyBox)"
+              case 1...:
+                  cell.dateLabel.text = "\(indexPath.row + 1 - NextNumberOfEmptyBox)"
+              case -1:
+                  cell.dateLabel.text = "\(indexPath.row + 1 - PreviousNumberOfEmptyBox)"
+              default:
+                  fatalError()
+
+              }
+              if Int(cell.dateLabel.text!)! < 1{
+                  cell.isHidden = true
+              }
+              
+              //show the weekend days in different color
+              
+              switch indexPath.row{
+              case 0,6,7,13,14,20,21,27,28,34,35: //the indexes of the collection view that matches with the weekend days in every month(they are always the same)
+                  
+                  if Int(cell.dateLabel.text!)! > 0{
+                      cell.dateLabel.textColor = UIColor.lightGray
+                      cell.dateImage.image = UIImage(named:"icon_setting")
+                  }
+              default:
+                  cell.dateImage.image = UIImage(named:"icon_cat")
+                  break
+              }
+              
+              //mark red the cell the shows the current date
+              //if currentMonth == Months[calendar.component(.month, from: date) - 1] && year == calendar.component(.year, from: date) && indexPath.row + 1 == day{
+              if currentMonth == Months[calendar.component(.month, from: date) - 1] && year == calendar.component(.year, from: date) && indexPath.row == day + 2{
+                  cell.backgroundColor = UIColor.red
+                  cell.dateLabel.textColor = UIColor.white
+                  cell.dateImage.image = UIImage(named:"")
+                  
+              }
+              //Compare array DaysHighLight with  dateString
+              dateStringDentist = "\(indexPath.row - PositionIndex + 1) \(currentMonth) \(year)"
+              print("FechaString \(dateStringDentist)")
+              
+              for i in 0..<DaysHighLight.count{
+                  if DaysHighLight[i] == dateStringDentist{
+                      cell.backgroundColor = UIColor.green
+                      cell.dateImage.image = UIImage(named:"")
+                      
+                  }
+              }
+              return cell
+          }
+          //for select date and pass to the other vc
+          /*func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+              
+              dateStringDentist = "\(indexPath.row - PositionIndex + 1) \(currentMonth) \(year)"
+              print(dateStringDentist)
+              
+              performSegue(withIdentifier: "DECalendarDate", sender: nil)
+              //highligthDate = indexPath.row
+              //collectionView.reloadData()
+              //print(indexPath.row)
+          }*/
+        @IBAction func backAction(_ sender: Any) {
+            self.dismiss(animated: true, completion: nil)
+        }
     }
-    
-}
+
+    //Width and height for collectionCell
+    extension DESeeCalendarViewController: UICollectionViewDelegateFlowLayout {
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            return CGSize(width: viewCalendar.frame.size.width/8, height: viewCalendar.frame.size.height/7.5)
+        }
+    }
+
+
